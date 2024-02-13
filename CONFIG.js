fb999441096f7dd03136200fdd2732c082029290
Eddz2 <148745990+Eddz2@users.noreply.github.com>
2024-02-13 16:09:51 +0800

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
   titleHeader: "To the love of my life!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
+    "Tatanggapin mo ba ako kahit isa akong aswang?",
   buttonYes: "Yes",
   buttonNo: "Hatdog ka ba?",
-  titleModar: "Cảm ơn cậu.",
+  titleModar: "My Beloved",
   descriptionModar:
     "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
 };
